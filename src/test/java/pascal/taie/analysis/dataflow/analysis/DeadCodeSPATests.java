6a9b7fa1eb2c7a18ee110550f838884d921ccf56
Lingyu Zhang <dz21330043@smail.nju.edu.cn>
2021-10-21 12:41:40 +0800

    pass SPA-tests A,B,C,D

---
+++ src/test/java/pascal/taie/analysis/dataflow/analysis/DeadCodeSPATests.java
@@ -0,0 +1,32 @@
+package pascal.taie.analysis.dataflow.analysis;
+
+import org.junit.Test;
+import pascal.taie.analysis.Tests;
+
+public class DeadCodeSPATests {
+
+    static void testDCD(String inputClass) {
+        Tests.testDFA(inputClass, "src/test/resources/SPA-tests/",
+                DeadCodeDetection.ID);
+    }
+
+    @Test
+    public void testA() {
+        testDCD("TestA");
+    }
+
+    @Test
+    public void testB() {
+        testDCD("TestB");
+    }
+
+    @Test
+    public void testC(){
+        testDCD("TestC");
+    }
+
+    @Test
+    public void TestD(){
+        testDCD("TestD");
+    }
+}
